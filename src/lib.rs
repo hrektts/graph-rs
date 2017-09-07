9b18c6270d727006f78264d9fa64cd757ec5ee13
Katsutoshi Horie <mps299792458@gmail.com>
2017-09-07 23:07:32 +0900

    Add visitor

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ extern crate slab;
 
 mod graph;
 mod incidence_list;
+mod visitor;
 
 pub use graph::{AdjacencyGraph, AdjacencyMatrixGraph, BidirectionalGraph, Directed, Directivity,
                 EdgeDescriptor, EdgeListGraph, Graph, IncidenceGraph, MutableGraph, Undirected,
                 VertexDescriptor, VertexListGraph};
 pub use incidence_list::{Edge, IncidenceList, IncidentEdges, IncidentVertices, Vertex};
+pub use visitor::{Event, Visitor};
---
+++ src/visitor.rs
@@ -0,0 +1,28 @@
+use graph::{Graph, EdgeDescriptor, VertexDescriptor};
+
+pub trait Visitor<G, T>
+where
+    G: Graph,
+{
+    fn visit(&mut self, e: &T, graph: &G);
+}
+
+pub enum Event {
+    InitializeVertex(VertexDescriptor),
+    StartVertex(VertexDescriptor),
+    DiscoverVertex(VertexDescriptor),
+    FinishVertex(VertexDescriptor),
+    ExamineVertex(VertexDescriptor),
+    ExamineEdge(EdgeDescriptor),
+    TreeEdge(EdgeDescriptor),
+    NonTreeEdge(EdgeDescriptor),
+    GrayTarget(EdgeDescriptor),
+    BlackTarget(EdgeDescriptor),
+    ForwardOrCrossEdge(EdgeDescriptor),
+    BackEdge(EdgeDescriptor),
+    FinishEdge(EdgeDescriptor),
+    EdgeRelaxed(EdgeDescriptor),
+    EdgeNotRelaxed(EdgeDescriptor),
+    EdgeMinimized(EdgeDescriptor),
+    EdgeNotMinimized(EdgeDescriptor),
+}
