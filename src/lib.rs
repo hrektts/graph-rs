6d4a6956d825cd0efc362e53ed59e00e27064dad
Katsutoshi Horie <mps299792458@gmail.com>
2017-09-08 00:08:10 +0900

    Add breadth first search

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
+extern crate fnv;
 extern crate slab;
 
 mod graph;
 mod incidence_list;
 mod visitor;
 
+mod breadth_first_search;
+
 pub use graph::{AdjacencyGraph, AdjacencyMatrixGraph, BidirectionalGraph, Directed, Directivity,
                 EdgeDescriptor, EdgeListGraph, Graph, IncidenceGraph, MutableGraph, Undirected,
                 VertexDescriptor, VertexListGraph};
 pub use incidence_list::{Edge, IncidenceList, IncidentEdges, IncidentVertices, Vertex};
 pub use visitor::{Event, Visitor};
+
+pub use breadth_first_search::Bfs;
