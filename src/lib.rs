c759c3ab94f6828ec3a33380dee5a6805269d232
Katsutoshi Horie <mps299792458@gmail.com>
2017-09-10 01:02:45 +0900

    Cosmetic change

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,11 @@ mod visitor;
 mod breadth_first_search;
 mod depth_first_search;
 
-pub use graph::{AdjacencyGraph, AdjacencyMatrixGraph, BidirectionalGraph, Directed, Directivity,
-                EdgeDescriptor, EdgeListGraph, Graph, IncidenceGraph, MutableGraph, Undirected,
-                VertexDescriptor, VertexListGraph};
+pub use graph::{Graph, AdjacencyGraph, AdjacencyMatrixGraph, BidirectionalGraph, EdgeListGraph,
+                IncidenceGraph, MutableGraph, VertexListGraph, EdgeDescriptor, VertexDescriptor,
+                Directivity, Directed, Undirected};
 pub use incidence_list::{Edge, IncidenceList, IncidentEdges, IncidentVertices, Vertex};
-pub use visitor::{Event, Visitor};
+pub use visitor::{Event, Visitor, DefaultVisitor};
 
 pub use breadth_first_search::Bfs;
 pub use depth_first_search::Dfs;
